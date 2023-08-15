f904fd69d64f48b3aa6bdbb0587a5fe9d4c839c7
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-15 18:57:25 +0900

    Feat: 영화 목록을 보여주는 영화 카드 컴포넌트 분리

---
+++ src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import rating_icon from "../../assets/rating-icon.png";
+import styles from "./movieCard.module.css";
+
+export default function MovieCard({ movie, sliderStyle }) {
+  const navigate = useNavigate();
+  const handleMovieClick = (movieId) => {
+    navigate(`/detail/${movieId}`);
+  };
+
+  return (
+    <article
+      className={styles.movie_box}
+      style={sliderStyle}
+      key={movie.title}
+      onClick={() => {
+        handleMovieClick(movie.id);
+      }}
+    >
+      <img
+        src={movie.medium_cover_image}
+        alt={`영화 ${movie.title} 포스터`}
+        className={styles.movie_img}
+        onError={(e) =>
+          (e.target.src = `https://dummyimage.com/230x345&text=${movie.title}`)
+        }
+      />
+      <p className={styles.movie_title}>{movie.title}</p>
+      <div className={styles.rating_box}>
+        <img
+          src={rating_icon}
+          alt="평점 별모양"
+          className={styles.rating_img}
+        />
+        <span>{movie.rating / 2}</span>
+      </div>
+    </article>
+  );
+}

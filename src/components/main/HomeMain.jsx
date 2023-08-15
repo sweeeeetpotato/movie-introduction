99184525c82b1796acc541b70a32fa8aeaae4d84
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-15 20:01:37 +0900

    Feat: 영화 목록 스타일(이미지슬라이드 or 그리드) 조건부 렌더링

--- src/components/main/HomeMain.jsx
+++ src/components/main/HomeMain.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCarousel from "components/movieCarousel/MovieCarousel";
+import MovieGrid from "components/movieGrid/MovieGrid";
 import caroucel_icon from "../../assets/caroucel-icon.png";
 import grid_icon from "../../assets/grid-icon.png";
 import styles from "./homeMain.module.css";
@@ -7,22 +8,32 @@ import { useAxiosMovies } from "customHook/useAxiosMovies";
 
 export default function HomeMain() {
   const API_URL =
-    "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&limit=50";
+    "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&page=1";
   const { data, loading, error } = useAxiosMovies(API_URL);
+  const [visibilityMode, setVisibilityMode] = useState("carousel");
 
   return loading ? (
     <p className={styles.loading_text}>Loading...</p>
   ) : (
     <main className={styles.main}>
       <div className={styles.button_box}>
-        <button type="button" className={`${styles.button} ${styles.left_btn}`}>
+        <span className={styles.title}>영화 목록</span>
+        <button
+          type="button"
+          className={`${styles.button} ${styles.left_btn}`}
+          onClick={() => setVisibilityMode("carousel")}
+        >
           <img
             src={caroucel_icon}
             alt="이미지 슬라이더 모드"
             className={styles.button_img}
           />
         </button>
-        <button type="button" className={styles.button}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => setVisibilityMode("grid")}
+        >
           <img
             src={grid_icon}
             alt="그리드 모드"
@@ -30,7 +41,10 @@ export default function HomeMain() {
           />
         </button>
       </div>
-      <MovieCarousel data={data} error={error} />
+      {visibilityMode === "carousel" && (
+        <MovieCarousel data={data} error={error} />
+      )}
+      {visibilityMode === "grid" && <MovieGrid data={data} error={error} />}
     </main>
   );
 }

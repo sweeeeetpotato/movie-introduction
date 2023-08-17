8f30bf18878a580c8f24a64dd403a5fc88c54366
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-18 00:49:22 +0900

    Update: 영화 상세페이지 디자인 변경 및 영화 추천 aside 컴포넌트 적용

--- src/components/movieDetail/MovieDetail.jsx
+++ src/components/movieDetail/MovieDetail.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAxiosMovies } from "customHook/useAxiosMovies";
+import rating_icon from "../../assets/rating-icon.png";
 import styles from "./movieDetail.module.css";
+import MovieSuggestions from "components/movieSuggestions/MovieSuggestions";
 
 export default function MovieDetail() {
   const { movieId } = useParams();
+  const [detail, setDetail] = useState({});
   const API_URL = `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`;
   const { data, loading, error } = useAxiosMovies(API_URL);
-  const [detail, setDetail] = useState({});
 
   useEffect(() => {
     data && setDetail(data.movie);
   }, [data]);
-
+  console.log(data?.movie);
   return (
     <section className={styles.container}>
       {error && <p className={styles.error_message}>{error}</p>}
@@ -26,16 +28,30 @@ export default function MovieDetail() {
             className={styles.movie_img}
           />
           <div className={styles.content_wrapper}>
-            <h3 className={styles.movie_title}>{detail.title_long}</h3>
-            <p className={styles.movieInfo}>
-              <span>Genres : </span>
-              {detail.genres}
-            </p>
-            <p className={styles.movieInfo}>
-              <span>Description : </span>
-              {detail.description_full}
-            </p>
+            <h3 className={styles.movie_title}>{detail.title}</h3>
+            <p>{detail.title_long}</p>
+            <div className={styles.runtime_rating_box}>
+              {detail.runtime !== 0 && <p>{detail.runtime + "분"}</p>}
+              <div className={styles.rating_box}>
+                <img
+                  src={rating_icon}
+                  alt="평점 별모양"
+                  className={styles.rating_img}
+                />
+                <p>{detail.rating / 2}</p>
+              </div>
+            </div>
+            <div>
+              {detail.genres?.map((genre, idx) => (
+                <span key={idx} className={styles.genre_label}>
+                  {genre}
+                </span>
+              ))}
+            </div>
+            <p className={styles.description_text}>{detail.description_full}</p>
           </div>
+
+          <MovieSuggestions movieId={movieId} />
         </>
       )}
     </section>

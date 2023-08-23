87cc0bdba35bb9b072293fa86fbb993c11e04c32
sweeeeetpotato <sweet_potato13@naver.com>
2023-08-23 18:48:18 +0900

    Update: 필요없는 코드 삭제 및 변경

--- src/components/movieDetail/MovieDetail.jsx
+++ src/components/movieDetail/MovieDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useAxiosMovies } from "customHook/useAxiosMovies";
 import rating_icon from "../../assets/rating-icon.png";
@@ -7,14 +7,9 @@ import MovieSuggestions from "components/movieSuggestions/MovieSuggestions";
 
 export default function MovieDetail() {
   const { movieId } = useParams();
-  const [detail, setDetail] = useState({});
   const API_URL = `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`;
   const { data, loading, error } = useAxiosMovies(API_URL);
 
-  useEffect(() => {
-    data && setDetail(data.movie);
-  }, [data]);
-  console.log(data?.movie);
   return (
     <section className={styles.container}>
       {error && <p className={styles.error_message}>{error}</p>}
@@ -23,34 +18,33 @@ export default function MovieDetail() {
       ) : (
         <>
           <img
-            src={detail.large_cover_image}
-            alt={`${detail.title} 포스터 이미지`}
+            src={data.movie.large_cover_image}
+            alt={`${data.movie.title} 포스터 이미지`}
             className={styles.movie_img}
           />
           <div className={styles.content_wrapper}>
-            <h3 className={styles.movie_title}>{detail.title}</h3>
-            <p>{detail.title_long}</p>
+            <h3 className={styles.movie_title}>{data.movie.title}</h3>
+            <p>{data.movie.title_long}</p>
             <div className={styles.runtime_rating_box}>
-              {detail.runtime !== 0 && <p>{detail.runtime + "분"}</p>}
+              {data.movie.runtime !== 0 && <p>{data.movie.runtime + "분"}</p>}
               <div className={styles.rating_box}>
                 <img
                   src={rating_icon}
                   alt="평점 별모양"
                   className={styles.rating_img}
                 />
-                <p>{detail.rating / 2}</p>
+                <p>{data.movie.rating / 2}</p>
               </div>
             </div>
             <div>
-              {detail.genres?.map((genre, idx) => (
+              {data.movie.genres?.map((genre, idx) => (
                 <span key={idx} className={styles.genre_label}>
                   {genre}
                 </span>
               ))}
             </div>
-            <p className={styles.description_text}>{detail.description_full}</p>
+            <p className={styles.description_text}>{data.movie.description_full}</p>
           </div>
-
           <MovieSuggestions movieId={movieId} />
         </>
       )}

d1b6a4c1425188af8ac010906fb47b44d362176a
sweeeeetpotato <sweet_potato13@naver.com>
2023-09-11 17:11:24 +0900

    Update : 검색 결과 페이지 전체적으로 수정

--- src/components/main/SearchMain.jsx
+++ src/components/main/SearchMain.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useMovieSWR } from "customHook/useMovieSWR";
 import SearchResultList from "components/searchResultList/SearchResultList";
-import styles from "./main.module.css";
+import styles from "./searchMain.module.css";
 
 export default function SearchResultMain() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const query = queryParams.get("query");
+  const API_URL = `https://yts.mx/api/v2/list_movies.json?minimum_rating=3&sort_by=rating&limit=50&query_term=${query}`;
+  const { data, isLoading, error } = useMovieSWR(API_URL);
+
   return (
     <main className={styles.main}>
-      <SearchResultList />
+      {error && (
+        <p className={`${styles.error_message} ${styles.text}`}>{error}</p>
+      )}
+      {isLoading ? (
+        <p className={`${styles.loading_text} ${styles.text}`}>검색중...</p>
+      ) : (
+        <SearchResultList data={data} query={query} />
+      )}
     </main>
   );
 }
--- src/components/searchResultList/SearchResultList.jsx
+++ src/components/searchResultList/SearchResultList.jsx
@@ -1,70 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useMovieSWR } from "customHook/useMovieSWR";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import MovieCard from "components/movieCard/MovieCard";
 import styles from "./searchResultList.module.css";
 
-export default function SearchResultList() {
-  const API_URL =
-    "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year";
+export default function SearchResultList({ data, query }) {
   const navigate = useNavigate();
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const query = queryParams.get("query");
-  const { data, isLoading, error } = useMovieSWR(API_URL);
-  const [moviesInfo, setMoviesInfo] = useState([]);
-  const [filteredMovies, setFilteredMovies] = useState([]);
 
   useEffect(() => {
-    data &&
-      setMoviesInfo(
-        data.movies.map((val) => [val.medium_cover_image, val.title, val.id])
-      );
-  }, [data]);
-
-  useEffect(() => {
-    setFilteredMovies(
-      moviesInfo.filter((movie) =>
-        movie[1].toLowerCase().includes(query.toLowerCase())
-      )
-    );
-  }, [moviesInfo, query]);
-
-  useEffect(() => {
-    // 검색어가 전부 지워졌을 경우에 대비하여 query를 props로 넘겨줌
     !query && navigate("/");
   });
 
-  const handleMovieClick = (movieId) => {
-    navigate(`/detail/${movieId}`);
-  };
-
   return (
     <section className={styles.container}>
-      {error && <p className={styles.error_message}>{error}</p>}
-      {isLoading ? (
-        <p className={styles.loading_text}>검색중...</p>
-      ) : !filteredMovies.length ? (
+      {!data?.movie_count ? (
         <p className={styles.result_text}>
           입력하신 검색어 '{query}'(와)과 일치하는 결과가 없습니다.
         </p>
       ) : (
-        filteredMovies.map((val) => (
-          <article
-            key={val[2]}
-            className={styles.movie_box}
-            onClick={() => {
-              handleMovieClick(val[2]);
-            }}
-          >
-            <img
-              src={val[0]}
-              alt={`${val[1]} 포스터 이미지`}
-              className={styles.movie_img}
-              onError={(e) => (e.target.parentNode.style.display = "none")}
-            />
-            <p className={styles.movie_title}>{val[1]}</p>
-          </article>
-        ))
+        <MovieCard mode={"search"} data={data} query={query} />
       )}
     </section>
   );

25b67cdb73598a64d369bfce6abc8bf9a1a7b4ec
sweeeeetpotato <sweet_potato13@naver.com>
2023-09-13 15:33:57 +0900

    Update : 영화목록 페이지네이션 useSWR으로 수정 및 프리패칭 추가

--- src/customHook/useMovieSWR.js
+++ src/customHook/useMovieSWR.js
@@ -1,7 +1,25 @@
-import useSWR from "swr";
+import axios from "axios";
+import useSWR, { preload } from "swr";
+
+const fetcher = async (API_URL) =>
+  await axios.get(API_URL).then((res) => res.data);
 
 export const useMovieSWR = (API_URL) => {
-  const { data, error, isLoading } = useSWR(API_URL);
+  const { data, error, isLoading } = useSWR(API_URL, fetcher);
+
+  return {
+    data: data?.data,
+    isLoading,
+    error,
+  };
+};
+
+export const useMovieListSWR = (API_URL, pageIndex) => {
+  const { data, error, isLoading } = useSWR(API_URL + pageIndex, fetcher);
+  
+  for (let i = 1; i < Math.ceil(data?.data.movie_count / 20); i++) {
+    preload(API_URL + (pageIndex + i), fetcher);
+  }
 
   return {
     data: data?.data,
--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { SWRConfig } from "swr";
-import axios from "axios";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <SWRConfig
-    value={{
-      fetcher: async (API_URL) =>
-        await axios.get(API_URL).then((res) => res.data),
-    }}
-  >
+  <SWRConfig>
     <App />
   </SWRConfig>
 );

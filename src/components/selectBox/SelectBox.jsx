94838c820e546016a0f1c19e847b67c92868b02c
sweeeeetpotato <sweet_potato13@naver.com>
2023-09-13 15:37:36 +0900

    Feat : 영화목록 정렬기준 셀렉트 박스 UI 구현

---
+++ src/components/selectBox/SelectBox.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import styles from "./selectBox.module.css";
+
+export default function SelectBox() {
+  const [isSelectBox, setSelectBox] = useState(false);
+  const [selectItemName, SetSelectItemName] = useState("정렬기준");
+
+  const handleSelectBox = () => {
+    isSelectBox ? setSelectBox(false) : setSelectBox(true);
+  };
+
+  const handleOptionItem = (optionItem) => {
+    SetSelectItemName(optionItem);
+    setSelectBox(false);
+  };
+
+  return (
+    <article className={styles.dropdown_selectbox}>
+      <button
+        className={`${styles.dropdown_btn} ${
+          isSelectBox && styles.active_dropdown_btn
+        }`}
+        type="button"
+        onClick={handleSelectBox}
+      >
+        {selectItemName}
+      </button>
+      <ul
+        className={`${styles.list_selectbox} ${
+          isSelectBox && styles.active_list_selectbox
+        }`}
+      >
+        <li>
+          <button
+            className={styles.optionItem}
+            type="button"
+            onClick={() => handleOptionItem("연도순(dafault)")}
+          >
+            연도순(dafault)
+          </button>
+        </li>
+        <li>
+          <button
+            className={styles.optionItem}
+            type="button"
+            onClick={() => handleOptionItem("인기순")}
+          >
+            인기순
+          </button>
+        </li>
+        <li>
+          <button
+            className={styles.optionItem}
+            type="button"
+            onClick={() => handleOptionItem("평점순")}
+          >
+            평점순
+          </button>
+        </li>
+        <li>
+          <button
+            className={styles.optionItem}
+            type="button"
+            onClick={() => handleOptionItem("제목순(오름차순)")}
+          >
+            제목순(오름차순)
+          </button>
+        </li>
+        <li>
+          <button
+            className={styles.optionItem}
+            type="button"
+            onClick={() => handleOptionItem("제목순(내림차순)")}
+          >
+            제목순(내림차순)
+          </button>
+        </li>
+      </ul>
+    </article>
+  );
+}

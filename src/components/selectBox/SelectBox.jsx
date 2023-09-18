952a37d921b5a1d4d0200ad93ad138a233ad6383
sweeeeetpotato <sweet_potato13@naver.com>
2023-09-18 18:46:11 +0900

    Update : 영화목록 페이지에서 페이지당 항목수 변경 기능을 위해 selectBox 코드 수정

--- src/components/selectBox/SelectBox.jsx
+++ src/components/selectBox/SelectBox.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import styles from "./selectBox.module.css";
 
-export default function SelectBox({ setSortOrder }) {
+export default function SelectBox({
+  selectName,
+  selectNameUpdate,
+  selectList,
+}) {
   const [isSelectBox, setSelectBox] = useState(false);
-  const [selectItemName, setSelectItemName] = useState("정렬기준");
-  const SORT = {
-    "연도순(dafault)": ["year", "desc"],
-    인기순: ["like_count", "desc"],
-    평점순: ["rating", "desc"],
-    "제목순(오름차순)": ["title", "asc"],
-    "제목순(내림차순)": ["title", "desc"],
-  };
   const handleSelectBox = () => {
     isSelectBox ? setSelectBox(false) : setSelectBox(true);
   };
 
   const handleOptionItem = (optionItem) => {
-    setSelectItemName(optionItem);
-    setSortOrder(SORT[optionItem]);
+    selectNameUpdate(optionItem);
     setSelectBox(false);
   };
 
@@ -30,58 +25,24 @@ export default function SelectBox({ setSortOrder }) {
         type="button"
         onClick={handleSelectBox}
       >
-        {selectItemName}
+        {selectName}
       </button>
       <ul
         className={`${styles.list_selectbox} ${
           isSelectBox && styles.active_list_selectbox
         }`}
       >
-        <li>
-          <button
-            className={styles.optionItem}
-            type="button"
-            onClick={() => handleOptionItem("연도순(dafault)")}
-          >
-            연도순(dafault)
-          </button>
-        </li>
-        <li>
-          <button
-            className={styles.optionItem}
-            type="button"
-            onClick={() => handleOptionItem("인기순")}
-          >
-            인기순
-          </button>
-        </li>
-        <li>
-          <button
-            className={styles.optionItem}
-            type="button"
-            onClick={() => handleOptionItem("평점순")}
-          >
-            평점순
-          </button>
-        </li>
-        <li>
-          <button
-            className={styles.optionItem}
-            type="button"
-            onClick={() => handleOptionItem("제목순(오름차순)")}
-          >
-            제목순(오름차순)
-          </button>
-        </li>
-        <li>
-          <button
-            className={styles.optionItem}
-            type="button"
-            onClick={() => handleOptionItem("제목순(내림차순)")}
-          >
-            제목순(내림차순)
-          </button>
-        </li>
+        {Object.keys(selectList).map((sort, index) => (
+          <li key={index}>
+            <button
+              className={styles.optionItem}
+              type="button"
+              onClick={() => handleOptionItem(sort)}
+            >
+              {sort}
+            </button>
+          </li>
+        ))}
       </ul>
     </article>
   );
